2e9bd88ccd00d517a27ef9c1d11e9713617d4746
Yura Synkulych <yura.synkulych@gmail.com>
2023-02-01 16:19:20 +0200

    Created bottom navbar

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 
-function App(): JSX.Element {
+export const HomeScreen = () => {
   return (
     <SafeAreaView>
-      <Text>Coffee Delivery App</Text>
+      <Text>Home screen</Text>
     </SafeAreaView>
   );
-}
-
-export default App;
+};
